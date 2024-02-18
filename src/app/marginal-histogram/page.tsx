5f1ca81c1511d29a2494ed4da586d510b827813d
Nathan Roark <nathanroark@protonmail.com>
2024-02-18 17:37:02 -0600

    trpc is broken

--- src/app/marginal-histogram/page.tsx
+++ src/app/marginal-histogram/page.tsx
@@ -1,14 +1,15 @@
-import { api } from "@/trpc/server";
+// import { api } from "@/trpc/server";
 import "./index.css";
 import Chart from "./Chart";
+import { data } from "./weatherData";
 
 export default async function MarginalHistogram() {
-  const dataset = await api.post.getWeatherData.query();
+  // const dataset = await api.post.getWeatherData.query();
 
   return (
     <div className="flex justify-center">
       <div className="flex w-full max-w-4xl flex-wrap items-center justify-center">
-        <Chart dataset={dataset} />
+        <Chart dataset={data} />
       </div>
     </div>
   );
